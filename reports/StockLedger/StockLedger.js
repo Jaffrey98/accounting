dfb5c275ff43ec518f84a3287802f4fe946416b1
Jaffrey Joy <jaffreyjoy@gmail.com>
2018-07-18 20:00:04 +0530

    [enh] add stock ledger report files

---
+++ reports/StockLedger/StockLedger.js
@@ -0,0 +1,19 @@
+const frappe = require('frappejs');
+
+class StockLedger {
+    async run(params) {
+        // const filters = {};
+        // if (params.fromDate) filters.date = ['>=', params.fromDate];
+        // if (params.toDate) filters.date = ['<=', params.toDate];
+
+        let data = await frappe.db.getAll({
+            doctype: 'StockEntry',
+            fields: ['date', 'sourceWarehouse', 'targetWarehouse'],
+            // filters: filters
+        });
+
+        return data;
+    }
+}
+
+module.exports = StockLedger;
